532b04655f2cea5d70295fccc2f323519972c948
Cassie Lebauer <cassie.lebauer@gmail.com>
2026-05-13 17:09:43 -0400

    added new conflig shortcuts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'pages': '/src/pages',
-      'context': '/src/context',
-      'components': '/src/components',
-      'assets': '/src/assets',
-      'data': '/src/data',
-//       'common-components': '/src/components/common',
-//       'styles':'/src/styles'
+      pages: path.resolve(__dirname, 'src/pages'),
+      context: path.resolve(__dirname, 'src/context'),
+      components: path.resolve(__dirname, 'src/components'),
+      assets: path.resolve(__dirname, 'src/assets'),
+      data: path.resolve(__dirname, 'src/data'),
+      features: path.resolve(__dirname, 'src/features'),
+      styles: path.resolve(__dirname, 'src/styles'),
     },
   },
 })
