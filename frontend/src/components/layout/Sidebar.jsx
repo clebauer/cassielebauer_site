d3c9a5079c46637f1927e4b89555f805c668ce85
Cassie Lebauer <cassie.lebauer@gmail.com>
2026-09-14 15:14:47 -0400

    updated

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -3,11 +3,16 @@ import "./Sidebar.css";
 
 export default function Sidebar() {
   const items = [
-    { id: "home", label: "Home", to: "/" },
-    { id: "resume", label: "Resume", to: "/resume" },
-    { id: "projects", label: "Projects", to: "/projects" },
-    { id: "skills", label: "Skills", to: "/skills" },
-    { id: "tools", label: "Tools", to: "/tools" },
+    // { id: "home", label: "Home", to: "/" },
+    // { id: "resume", label: "Resume", to: "/resume" },
+    { id: "there", label: "there", to: "/" },
+    { id: "will", label: "will", to: "/" },
+    { id: "be", label: "be", to: "/" },
+    { id: "more", label: "more", to: "/" },
+    { id: "soon", label: "soon", to: "/" },
+//     { id: "projects", label: "Projects", to: "/projects" },
+//     { id: "skills", label: "Skills", to: "/skills" },
+//     { id: "tools", label: "Tools", to: "/tools" },
   ];
 
   return (
